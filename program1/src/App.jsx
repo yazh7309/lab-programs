84bf958f7f0cc8c6cbea2e3ba40832aef877ca00
yazh7309 <yazhiniraj7309@gmail.com>
2026-02-24 11:44:35 +0530

    programs

--- program1/src/App.jsx
+++ program1/src/App.jsx
@@ -3,13 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ThemeToggle from './component/Theme/ThemeToggle.jsx'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <ThemeToggle />
-  )
+    <BrowserRouter>
+
+      <Link to="/">Home</Link> |
+      <Link to="/about">About</Link>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
 }
 
 export default App;
